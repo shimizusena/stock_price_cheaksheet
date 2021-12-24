73e474f255c0c54e65aa31a0587c46902e7d91ed
shimizusena <senarugby0813@gmail.com>
2021-12-24 21:56:36 +0900

    完成

---
+++ .ipynb_checkpoints/main-checkpoint.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import yfinance as yf
+import altair as alt
+
+
+days = 20
+tickers = {
+    "apple":"AAPL",
+    "facebook":"FB",
+    "google":"GOOGL",
+    "microsoft":"MSFT",
+    "nexflix":"NFLX",
+    "amazon":"AMZN"
+}
+def get_data(days, tickers):
+    df = pd.DataFrame()
+    companys = ["apple","facebook"]
+    for company in tickers.keys():
+        tkr = yf.Ticker(tickers[company])
+        hist = tkr.history(period=f"{days}d")
+        hist.index = hist.index.strftime("%d %B %Y")
+        hist = hist[["Close"]]
+        hist.columns = [company]
+        hist.head()
+        hist = hist.T
+        hist.index.name = "Name"
+        df = pd.concat([df, hist])
+    return df
+
+
+
+print(get_data(days, tickers))
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,11 +1,4 @@
-import pandas as pd
-import yfinance as yf
-import altair as alt
-import streamlit as st
-
-st.title('米国株価可視化アプリ')
-
-st.sidebar.write("""
+sidebar.write("""
 # GAFA株価
 こちらは株価可視化ツールです。以下のオプションから表示日数を指定できます。
 """)
